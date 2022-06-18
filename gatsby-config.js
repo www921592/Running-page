fded1476b46258501ab0fb3a1c3a7be26c7fd070
www921592 <43979137+www921592@users.noreply.github.com>
2022-06-18 15:50:11 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   pathPrefix: '/', // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'Kerpler921 Running Page',
-    siteUrl: 'https://yihong.run',
+    siteUrl: 'https://running-page-pied.vercel.app/',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
+        url: 'https://running-page-pied.vercel.app/',
       },
       {
         name: 'About',
